6158222ac0bcef5987132b40ab96b6152f44008b
Nick Rapattoni <rapattonimusic@me.com>
2023-10-26 09:12:57 -0700

    More stuff

---
+++ tests/theTests.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test'
+
+test("Test 1", async ({ page }) => {
+
+  await page.goto('https://www.google.com')
+});
+
+test("Test 2", async ({ page }) => {
+
+  await page.goto('https://www.google.com')
+});
+
+test("Test 3", async ({ page }) => {
+
+  await page.goto('https://www.google.com')
+});
--- tests/theTests.ts
+++
@@ -1,7 +0,0 @@
-import endpoint from "./configTypes"
-import { test, expect } from '@playwright/test'
-
-test("Test 1", async ({ page }) => {
-
-  await page.goto('https://www.google.com')
-});
