dac6e4fe983b3af7db2bb1305f7c7e543a3b2e96
Nick Rapattoni <rapattonimusic@me.com>
2023-10-26 08:45:46 -0700

    Modifying

--- tests/theTests.ts
+++ tests/theTests.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test'
 
 test("Test 1", async ({ page }) => {
 
-  await page.goto('https://www.google.com')
+  await page.goto('https://www.google.com)
 });
